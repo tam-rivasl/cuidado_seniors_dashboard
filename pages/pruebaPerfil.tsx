8bddc3c1ae4311f2ddaeb158fdb4655307e6b1fa
tamara <tam.rivas@duocuc.cl>
2023-11-25 01:28:07 -0300

    fix: perfil?

---
+++ pages/pruebaPerfil.tsx
@@ -0,0 +1,188 @@
+import React, { useEffect, useState } from "react";
+import {
+  Descriptions,
+  notification,
+  Row,
+  Col,
+  Layout,
+  theme,
+  Button,
+  Modal,
+} from "antd";
+import MenuComponent from "../components/menu"; // Asegúrate de importar el componente del menú
+import type { DescriptionsProps } from "antd";
+import moment from "moment";
+
+const { Sider, Content, Footer } = Layout;
+
+export default function UserProfile() {
+  const [user, setUser] = useState({});
+  const [userId, setUserId] = useState("" as any);
+  const [selectedKeys, setSelectedKeys] = useState<string[]>(["1"]);
+  const [collapsed, setCollapsed] = useState(false); // Variable de estado para el colapso del menú
+
+  useEffect(() => {
+    const userId = localStorage.getItem("userId");
+    if (userId) {
+      console.log(userId);
+      setUserId(parseInt(userId));
+    }
+  }, []);
+
+  useEffect(() => {
+    getUserData();
+  }, [userId]);
+
+  const handleMenuSelect = (keys: string[]) => {
+    setSelectedKeys(keys);
+  };
+
+  const getUserData = () => {
+    const requestBody = {
+      userId: userId,
+    };
+    fetch("/api/getUserById", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(requestBody),
+    })
+      .then(async (response) => {
+        if (response.ok) {
+          const data = await response.json();
+          console.log(data, "dataa");
+          setUser(data);
+        } else {
+          response.json().then((data) => {
+            showErrorNotification(
+              data.message || "Error al cargar el listado de citas"
+            );
+          });
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+        showErrorNotification(error.message || "Error al consultar API");
+      });
+  };
+
+  const showErrorNotification = (error: any) => {
+    notification.error({
+      message: "Error",
+      description: error,
+      placement: "topRight",
+    });
+  };
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const items: DescriptionsProps["items"] = [
+    {
+      label: "Nombre",
+      children: user.firstName || "No Data",
+    },
+    {
+      label: "Apellido",
+      children: user.lastName || "N/A",
+    },
+    {
+      label: "Genero",
+      children: user.gender || "N/A",
+    },
+    {
+      label: "Edad",
+      children: user.age || "N/A",
+    },
+    {
+      label: "Rut",
+      children: user.identificationNumber || "N/A",
+    },
+    {
+      label: "Fecha de Nacimiento",
+      children: moment(user.birthDate).format("DD/MM/YYYY")  || "N/A",
+    },
+    {
+      label: "Número de Teléfono",
+      children: user.phoneNumber || "N/A",
+    },
+    {
+      label: "Email",
+      children: user.email || "N/A",
+    },
+    {
+      label: "Direccion",
+      children: user.address || "N/A",
+    },
+
+    {
+      label: "Direccion",
+      children: user.address || "N/A",
+    },
+  ];
+
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+
+  return (
+    <Layout style={{ minHeight: "100vh" }}>
+      <Sider
+        collapsible
+        collapsed={collapsed}
+        onCollapse={(value) => setCollapsed(value)}
+      >
+        <div className="demo-logo-vertical" />
+        <MenuComponent
+          selectedKeys={selectedKeys}
+          onMenuSelect={handleMenuSelect}
+        />
+      </Sider>
+      <Layout>
+        <Content>
+          <Row justify="center">
+            <div
+              style={{
+                boxShadow: "0 4px 8px 0 rgba(0,0,0,0.2)",
+                borderRadius: "10px",
+                padding: "20px",
+                margin: "60px",
+                marginBlock: "50px",
+              }}
+            >
+              <Descriptions
+                title="Mi Perfil"
+                bordered
+                column={{ xs: 1, sm: 1, md: 4, lg: 3, xl: 1, xxl: 4 }}
+                items={items}
+              />
+            </div>
+          </Row>
+
+          <>
+            <Button type="primary" onClick={showModal}>
+              Open Modal
+            </Button>
+            <Modal
+              title="Basic Modal"
+              open={isModalOpen}
+              onOk={handleOk}
+              onCancel={handleCancel}
+            >
+              <p>Some contents...</p>
+              <p>Some contents...</p>
+              <p>Some contents...</p>
+            </Modal>
+          </>
+        </Content>
+        <Footer style={{ textAlign: "center" }}></Footer>
+      </Layout>
+    </Layout>
+  );
+}
